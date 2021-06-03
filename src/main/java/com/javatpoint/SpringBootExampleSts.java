df307930ba7a7a576e8c741fadf256d0aa101187
Sharath Dandin <56582780+SharathDandin001@users.noreply.github.com>
2021-06-03 08:55:47 +0530

    Fixed AB#5

--- src/main/java/com/javatpoint/SpringBootExampleSts.java
+++ src/main/java/com/javatpoint/SpringBootExampleSts.java
@@ -6,6 +6,7 @@ public class SpringBootExampleSts
 {
 public static void main(String[] args)
 {  
-SpringApplication.run(SpringBootExampleSts.class, args);  
+SpringApplication.run(SpringBootExampleSts.class, args); 
+  system.out.println("TEST");
 } 
 }
